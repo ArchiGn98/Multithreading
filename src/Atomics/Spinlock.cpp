478d907beb8087c8939c8f1610d9e7728d3ac189
AHnoinskyi <ahnoinskyi@luxoft.com>
2020-11-12 14:26:03 +0200

    Project restructure and tests of std::atomic_flag, std::mutex

---
+++ src/Atomics/Spinlock.cpp
@@ -0,0 +1,128 @@
+#include "gtest/gtest.h"
+
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+class SpinlockAtomicFlag
+{
+	// set to false
+	std::atomic_flag flag = ATOMIC_FLAG_INIT;
+public:
+	void lock()
+	{
+		// set value back to true and return previous state
+		while ( flag.test_and_set() );
+	}
+
+	void unlock()
+	{
+		// set value to false
+		flag.clear();
+	}
+};
+
+SpinlockAtomicFlag spin;
+std::mutex mut;
+
+// using our custom lock cpu load reaches to high level if sleap is active
+void mergeVectorsSpinlock(std::vector<int>& vec1, const std::vector<int>& vec2)
+{
+	spin.lock();
+	for (const auto& el : vec2)
+	{
+		vec1.push_back(el);
+	}
+	//std::this_thread::sleep_for(std::chrono::milliseconds(20000));
+	spin.unlock();
+}
+
+// using mutex cpu high-load is not observed if sleap is active
+void mergeVectorsMutex(std::vector<int>& vec1, const std::vector<int>& vec2)
+{
+	mut.lock();
+	for (const auto& el : vec2)
+	{
+		vec1.push_back(el);
+	}
+	//std::this_thread::sleep_for(std::chrono::milliseconds(20000));
+	mut.unlock();
+}
+
+void mergeVectors(std::vector<int>& vec1, const std::vector<int>& vec2)
+{
+	for (const auto& el : vec2)
+	{
+		vec1.push_back(el);
+	}
+}
+
+TEST(Atomics, MergeVectorsSpinlockAtomicFlag)
+{
+	std::vector<int> vec1 { 1, 2, 3, 4 };
+	std::vector<int> vec2 { 5, 6, 7, 8 };
+	std::vector<int> vec3 { 9, 10, 11, 12 };
+
+	// thread 1 will be first one
+	std::vector<int> expected1 { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; 
+
+	// thread 2 will be first one
+	std::vector<int> expected2 { 1, 2, 3, 4, 9, 10, 11, 12, 5, 6, 7, 8 };
+
+	std::thread t1(mergeVectorsSpinlock, std::ref(vec1), vec2);
+	std::thread t2(mergeVectorsSpinlock, std::ref(vec1), vec3);
+
+	t1.join();
+	t2.join();
+
+	bool isEqual = (vec1 == expected1) | (vec1 == expected2);
+
+	EXPECT_EQ(true, isEqual);
+}
+
+TEST(Atomics, MergeVectorsMutex)
+{
+	std::vector<int> vec1{ 1, 2, 3, 4 };
+	std::vector<int> vec2{ 5, 6, 7, 8 };
+	std::vector<int> vec3{ 9, 10, 11, 12 };
+	
+	// thread 1 will be first one
+	std::vector<int> expected1{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+	// thread 2 will be first one
+	std::vector<int> expected2{ 1, 2, 3, 4, 9, 10, 11, 12, 5, 6, 7, 8 };
+
+	std::thread t1(mergeVectorsMutex, std::ref(vec1), vec2);
+	std::thread t2(mergeVectorsMutex, std::ref(vec1), vec3);
+
+	t1.join();
+	t2.join();
+
+	bool isEqual = (vec1 == expected1) | (vec1 == expected2);
+
+	EXPECT_EQ(true, isEqual);
+}
+
+TEST(Atomics, MergeVectors)
+{
+	std::vector<int> vec1{ 1, 2, 3, 4 };
+	std::vector<int> vec2{ 5, 6, 7, 8 };
+	std::vector<int> vec3{ 9, 10, 11, 12 };
+	
+	// thread 1 will be first one
+	std::vector<int> expected1{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+	// thread 2 will be first one
+	std::vector<int> expected2{ 1, 2, 3, 4, 9, 10, 11, 12, 5, 6, 7, 8 };
+
+	std::thread t1(mergeVectors, std::ref(vec1), vec2);
+	std::thread t2(mergeVectors, std::ref(vec1), vec3);
+
+	t1.join();
+	t2.join();
+	
+	// shared value vec1 is not locked => undefined behavior because of data-raise
+	//bool isEqual = (vec1 == expected1) | (vec1 == expected2);
+	//EXPECT_EQ(vec1, expected);
+}
---
+++ src/MemoryModel.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+struct S
+{
+    char a;         // memory location #1
+    int b : 5;      // memory location #2
+    int c : 11,     // memory location #2 (continued)
+        : 0,        // Force alignment to next boundary.
+        d : 8;      // memory location #3
+    int e;          // memory location #4
+    double f;       // memory location #5
+    std::string g;  // several memory locations
+};
